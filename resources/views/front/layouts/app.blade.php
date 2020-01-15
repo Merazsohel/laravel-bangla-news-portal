c6570a1459cbb3fbe21879c81386347d4eb6b590
Meraz <merazhossain64@gmail.com>
2020-01-15 10:25:01 +0600

    helpers function added

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -1,21 +1,4 @@
-<?php
-class BanglaConverter {
-    public static $bn = array("১", "২", "৩", "৪", "৫", "৬", "৭", "৮", "৯", "০");
-    public static $en = array("1", "2", "3", "4", "5", "6", "7", "8", "9", "0");
-
-    public static function bn2en($number) {
-        return str_replace(self::$bn, self::$en, $number);
-    }
-
-    public static function en2bn($number) {
-        return str_replace(self::$en, self::$bn, $number);
-    }
-}
-
-
-?>
-
-    <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <title>{{$settings->site_title}}</title>
